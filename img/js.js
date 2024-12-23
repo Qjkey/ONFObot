0e62c92dcac2a7b29d8be61e71ddcde71a7279be
GroAlexPixel <164654123+Qjkey@users.noreply.github.com>
2024-12-23 17:12:34 +0300

    Update js.js

--- img/js.js
+++ img/js.js
@@ -1,11 +1,11 @@
-var tg = window.Telegram.WebApp;
+var tg = window.Telegram.MiniApp;
 var BackButton = WebApp.BackButton;
 
 BackButton.show();
 
-BackButton.onClick(function() {  
-    BackButton.hide();
-});
-WebApp.onEvent('backButtonClicked', function() {      
-    BackButton.hide();          
-});
+// BackButton.onClick(function() {  
+//     BackButton.hide();
+// });
+// WebApp.onEvent('backButtonClicked', function() {      
+//     BackButton.hide();          
+// });
