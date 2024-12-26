9b49f63004550cceaef790d5eb9786801976d71b
GroAlexPixel <164654123+Qjkey@users.noreply.github.com>
2024-12-26 19:28:33 +0300

    Update js.js

--- img/js.js
+++ img/js.js
@@ -1,8 +1,20 @@
 var tg = window.Telegram.WebApp;
 var BackButton = tg.BackButton;
+var MainButton = tg.MainButton;
 
 BackButton.show();
+MainButton.show();
+
+tg.MainButton.text = "Читать";
+// tg.MainButton.color = tg.ThemeParams.button_color;
+tg.MainButton.color = 'F8AFA0';
+
 BackButton.onClick(function() {
     BackButton.hide();
     window.location = 'https://qjkey.github.io/ONFObot/books.html'
+});
+
+MainButton.onClick(function() {
+  MainButton.hide();    
+  document.write("228")
 });
