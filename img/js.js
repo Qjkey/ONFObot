a35b230792fa5c4cd1df9b4f45fc48743a36583b
GroAlexPixel <164654123+Qjkey@users.noreply.github.com>
2024-12-26 19:32:49 +0300

    Update js.js

--- img/js.js
+++ img/js.js
@@ -6,15 +6,13 @@ BackButton.show();
 MainButton.show();
 
 tg.MainButton.text = "Читать";
-// tg.MainButton.color = tg.ThemeParams.button_color;
-tg.MainButton.color = 'F8AFA0';
 
 BackButton.onClick(function() {
     BackButton.hide();
     window.location = 'https://qjkey.github.io/ONFObot/books.html'
 });
 
-MainButton.onClick(function() {
-  MainButton.hide();    
-  document.write("228")
-});
+// MainButton.onClick(function() {
+//   MainButton.hide();    
+//   document.write("228")
+// });
