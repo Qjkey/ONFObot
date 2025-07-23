692bd86cc8a9d15b1c3d899c9da5e205de5ee70a
GroAlexPixel <164654123+Qjkey@users.noreply.github.com>
2025-07-23 13:19:11 +0300

    Update js.js

--- img/js.js
+++ img/js.js
@@ -13,23 +13,11 @@ tg.onEvent('backButtonClicked', function() {
     BackButton.hide();
 });
 
-app.post("/create-invoice", async (req, res) => {
+tg.createInvoiceLink(
+    "Title", "Some description", "{}", "XTR", [{ amount: 1, label: "Diamond"
+);
 
-  const invoiceLink = await botApi.createInvoiceLink(
-    "Title", //title
-    "Some description", //description
-    "{}", //payload
-    "", // For Telegram Stars payment this should be empty
-    "XTR", //currency
-    [{ amount: 1, label: "Diamond" }], //prices
-  );
-
-  res.json({ invoiceLink });
-});
-
-const { invoiceLink } = await useGetInvoiceLink();
-
-WebApp.openInvoice(invoiceLink, (status) => {
+tg.openInvoice(function() {
   if (status === "paid") {
     alert('Спс');
   }
