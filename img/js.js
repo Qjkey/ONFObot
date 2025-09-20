2f50f69c33c8ab8dcf3c5a185566185f1191744d
GroAlexPixel <164654123+Qjkey@users.noreply.github.com>
2025-09-20 20:03:38 +0300

    Update js.js

--- img/js.js
+++ img/js.js
@@ -6,7 +6,7 @@ BackButton.show();
 
 BackButton.onClick(function() {
     BackButton.hide();
-    tg.exitFullscreen() 
+    tg.exitFullscreen();
     window.location = 'https://qjkey.github.io/ONFObot/books.html';
 });
 tg.onEvent('backButtonClicked', function() {
